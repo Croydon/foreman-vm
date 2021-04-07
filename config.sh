31771c0a1933e168dcd8fff36fcad87310767393
Cr0ydon <git@cr0ydon.com>
2021-04-07 17:53:36 +0200

    Several Foreman config fixes

--- config.sh
+++ config.sh
@@ -10,11 +10,15 @@ ${hammer} architecture create --name "arm64"
 
 ${hammer} medium create --name "Ubuntu ARM ports" --path "http://ports.ubuntu.com" --os-family "Debian"
 
-${hammer} os create --name "Ubuntu" --description "Ubuntu 20.04 LTS" --release-name "focal" --major "20" --minor "04" --family "Debian" --password-hash "SHA256" --architectures "arm64,x86_64" --medium "Ubuntu ARM ports"
+${hammer} os create --name "Ubuntu" --description "Ubuntu 20.04 LTS ARM" --release-name "focal" --major "20" --minor "04" --family "Debian" --password-hash "SHA256" --architectures "arm64" --media "Ubuntu ARM ports" --partition-tables "Preseed default"
 
-${hammer} clone --name "Preseed default PXELinux" --new-name "Pilab Preseed default PXELinux"
+${hammer} template clone --name "Preseed default PXELinux" --new-name "Pilab Preseed default PXELinux"
 
-${hammer} clone --name "Preseed default PXEGrub2" --new-name "Pilab Preseed default PXEGrub2"
+${hammer} template clone --name "Preseed default PXEGrub2" --new-name "Pilab Preseed default PXEGrub2"
+
+${hammer} template add-operatingsystem --name "Pilab Preseed default PXELinux" --operatingsystem "Ubuntu 20.04 LTS ARM"
+
+${hammer} template add-operatingsystem --name "Pilab Preseed default PXEGrub2" --operatingsystem "Ubuntu 20.04 LTS ARM"
 
 # ${hammer} template add-operatingsystem --name "Pilab Preseed default PXELinux" --operatingsystem "Ubuntu 20.04 LTS"
 

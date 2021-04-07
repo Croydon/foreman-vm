598e196d968592ab97621c4c456d5a973d2596da
Cr0ydon <git@cr0ydon.com>
2021-04-07 19:50:25 +0200

    config.sh: add subnet creation

--- config.sh
+++ config.sh
@@ -10,10 +10,13 @@ ${hammer} architecture create --name "arm64"
 
 ${hammer} medium create --name "Ubuntu ARM ports" --path "http://ports.ubuntu.com" --os-family "Debian"
 
+# On a fresh Foreman installation this will be the second OS added, so we assume from here one, that it will have the ID 2
 ${hammer} os create --name "Ubuntu" --description "Ubuntu 20.04 LTS ARM" --release-name "focal" --major "20" --minor "04" --family "Debian" --password-hash "SHA256" --architectures "arm64" --media "Ubuntu ARM ports" --partition-tables "Preseed default"
 
+# On a fresh Foreman installation this will be the 127th template added
 ${hammer} template clone --name "Preseed default PXELinux" --new-name "Pilab Preseed default PXELinux"
 
+# Template ID 128
 ${hammer} template clone --name "Preseed default PXEGrub2" --new-name "Pilab Preseed default PXEGrub2"
 
 ${hammer} template add-operatingsystem --name "Pilab Preseed default PXELinux" --operatingsystem "Ubuntu 20.04 LTS ARM"
@@ -24,4 +27,16 @@ ${hammer} template add-operatingsystem --name "Pilab Preseed default PXEGrub2" -
 
 # ${hammer} template add-operatingsystem --name "Pilab Preseed default PXEGrub2" --operatingsystem "Ubuntu 20.04 LTS"
 
-${hammer} os add-provisioning-template --title "Ubuntu 20.04 LTS" --provisioning-template-search "Pilab"
+${hammer} os add-provisioning-template --title "Ubuntu 20.04 LTS ARM" --provisioning-template-search "Pilab"
+
+${hammer} os set-default-template --id 2 --provisioning-template-id 127
+
+${hammer} os set-default-template --id 2 --provisioning-template-id 128
+
+###
+### Create Subnet (network configuration information for provisioning)
+###
+
+# Only one Smart Proxy is implicit created, so the httpboot-id is assumed to be 1
+# Hammer does allow to use names for DHCP and TFTP, but not HTTPBOOT for some reason
+${hammer} subnet create --name "Pilab Subnet" --network-type "IPv4" --network "${network_address}" --prefix "${network_prefix}" --mask "${network_mask}" --from "${network_from}" --to "${network_to}" --ipam "${network_ipam}" --boot-mode "${network_bootmode}" --domain "${network_domain}" --dhcp "${fqdn}" --tftp "${fqdn}" --httpboot-id "1"
--- variables.sh
+++ variables.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 
-# Config
+# General Config
 fqdn="foreman.fritz.box"
+network_domain="fritz.box"
 ip="192.168.188.126"
 network_interface="enp0s9"
 # network_interface=$(nmcli -f DEVICE device | grep enp | tr -d [:space:])
 foreman_username="pilab"
 foreman_password="pilab"
+
+
+# Subnet Config
+network_address="192.168.188.1"
+network_from="192.168.188.50"
+network_to="192.168.188.120"
+network_prefix="24"
+network_mask="255.255.255.0"
+network_ipam="DHCP"
+network_bootmode="DHCP"

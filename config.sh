086eb853c9f14a27ed467c0b77ab82cd07d227a4
Cr0ydon <git@cr0ydon.com>
2021-04-20 02:51:21 +0200

    config.sh: create x86_64 Ubuntu host to verify setup
    Create  an empty VirtualBox x86_64 VM with the MAC address as specified in host_0_mac.
    
    config.sh will create the host in Foreman. This simple setup helps to verify that the network settings and the  Foreman configuration and Ubuntu VM works in general.
    
    If the network boot works, you can move on to more comlicated configurations.

--- config.sh
+++ config.sh
@@ -13,6 +13,10 @@ ${hammer} medium create --name "Ubuntu ARM ports" --path "http://ports.ubuntu.co
 # On a fresh Foreman installation this will be the second OS added, so we assume from here one, that it will have the ID 2
 ${hammer} os create --name "Ubuntu" --description "Ubuntu 20.04 LTS ARM" --release-name "focal" --major "20" --minor "04" --family "Debian" --password-hash "SHA256" --architectures "arm64" --media "Ubuntu ARM ports" --partition-tables "Preseed default"
 
+# For Ubuntu 18.04 we need to add manually the media and partition tables, the rest it auto configured based
+# on the environment of this VM
+${hammer} os update --id 1 --media "Ubuntu mirror" --partition-tables "Preseed default"
+
 # On a fresh Foreman installation this will be the 127th template added
 ${hammer} template clone --name "Preseed default PXELinux" --new-name "Pilab Preseed default PXELinux"
 
@@ -23,20 +27,33 @@ ${hammer} template add-operatingsystem --name "Pilab Preseed default PXELinux" -
 
 ${hammer} template add-operatingsystem --name "Pilab Preseed default PXEGrub2" --operatingsystem "Ubuntu 20.04 LTS ARM"
 
-# ${hammer} template add-operatingsystem --name "Pilab Preseed default PXELinux" --operatingsystem "Ubuntu 20.04 LTS"
+${hammer} template add-operatingsystem --name "Pilab Preseed default PXELinux" --operatingsystem "Ubuntu 18.04.5 LTS"
 
-# ${hammer} template add-operatingsystem --name "Pilab Preseed default PXEGrub2" --operatingsystem "Ubuntu 20.04 LTS"
+${hammer} template add-operatingsystem --name "Pilab Preseed default PXEGrub2" --operatingsystem "Ubuntu 18.04.5 LTS"
 
 ${hammer} os add-provisioning-template --title "Ubuntu 20.04 LTS ARM" --provisioning-template-search "Pilab"
+
+${hammer} os add-provisioning-template --title "Ubuntu 18.04.5 LTS" --provisioning-template-search "Pilab"
 
 ${hammer} os set-default-template --id 2 --provisioning-template-id 127
 
 ${hammer} os set-default-template --id 2 --provisioning-template-id 128
 
+${hammer} os set-default-template --id 1 --provisioning-template-id 127
+
+${hammer} os set-default-template --id 1 --provisioning-template-id 128
+
 ###
 ### Create Subnet (network configuration information for provisioning)
 ###
 
-# Only one Smart Proxy is implicit created, so the httpboot-id is assumed to be 1
-# Hammer does allow to use names for DHCP and TFTP, but not HTTPBOOT for some reason
-${hammer} subnet create --name "Pilab Subnet" --network-type "IPv4" --network "${network_address}" --prefix "${network_prefix}" --mask "${network_mask}" --from "${network_from}" --to "${network_to}" --ipam "${network_ipam}" --boot-mode "${network_bootmode}" --domain "${network_domain}" --dhcp "${fqdn}" --tftp "${fqdn}" --httpboot-id "1"
+${hammer} subnet create --name "Pilab Subnet" --network-type "IPv4" --network "${network_address}" --prefix "${network_prefix}" --mask "${network_mask}" --from "${network_from}" --to "${network_to}" --ipam "${network_ipam}" --boot-mode "${network_bootmode}" --domains "${network_domain}" --dhcp "${fqdn}" --tftp "${fqdn}"
+
+###
+### Create exmplary Raspberry Host
+###
+${hammer} host create --name "Pilab Pi 1" --architecture "arm64" --operatingsystem "Ubuntu 20.04 LTS" --medium "Ubuntu ARM ports" --partition-table "Preseed default" --pxe-loader "PXELinux BIOS" --location-title "Default Location" --organization-title "Default Organization" --root-password "${host_root_pw}"  --interface "type=${host_1_type},mac=${host_1_mac},ip=${host_1_ip},domain_id=1,subnet_id=1,managed=true,provision=true"
+
+
+### x86_64 host to verify setup
+${hammer} host create --name "Ubuntu VM Test" --architecture "x86_64" --operatingsystem "Ubuntu 18.04.5 LTS" --medium "Ubuntu mirror" --partition-table "Preseed default" --pxe-loader "PXELinux BIOS" --location-title "Default Location" --organization-title "Default Organization" --root-password "${host_root_pw}"  --interface "type=${host_0_type},mac=${host_0_mac},ip=${host_0_ip},domain_id=1,subnet_id=1,managed=true,provision=true"
--- variables.sh
+++ variables.sh
@@ -19,3 +19,16 @@ network_prefix="24"
 network_mask="255.255.255.0"
 network_ipam="DHCP"
 network_bootmode="DHCP"
+
+# Host Config
+host_root_pw="12345678"
+
+# x86_64 Test VM
+host_0_type="interface"
+host_0_mac="080027D4A579"
+host_0_ip="192.168.188.100"
+
+# Pi 1
+host_1_type="interface"
+host_1_mac="dca632308763"
+host_1_ip="192.168.188.101"
